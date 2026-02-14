977d0ac4580dd752276c20bce7897f7c0617fbd1
vodish <vodish@yandex.ru>
2026-02-14 21:32:53 +0300

    upd

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,48 @@
 <script lang="ts">
-  import { questions } from "./questions/questions.svelte";
   import Timer from "./Timer.svelte";
-
-  let rand = $state(-1);
-  let topic = $derived(questions[rand]);
-
-  let stat = $state<{ [k: number]: number }>({});
-  let count = $derived(stat[rand]);
-
-  function re() {
-    let arr = Object.keys(questions).filter((it) => it != String(rand));
-    rand = Number(arr[Math.floor(Math.random() * arr.length)]);
-    stat[rand] = stat[rand] ? (stat[rand] += 1) : 1;
-  }
-  re();
+  import yaStore from "./questions.yaml?raw";
 
   let btn: HTMLButtonElement;
   $effect(() => btn.focus());
+
+  let arr = yaStore.split(new RegExp("\r?\n")).filter((v) => v[0] == "-");
+
+  let source = $state([...arr, ...arr]);
+  let target = $state<string[]>([]);
+  let topic = $derived(
+    target[0]
+      ?.replace(/^- /, "") // отрезать слева знаки
+      .replace(/ (\S{1,2})\s+(?=\S{3,})/gi, " $1\u00A0"), // типографика
+  );
+
+  function next() {
+    if (source.length == 0) {
+      return;
+    }
+
+    let cur = target[0] || "";
+    let r = Math.floor(Math.random() * source.length);
+    for (; cur == source[r]; r = Math.floor(Math.random() * source.length)) {}
+
+    let itm = source[r];
+    target = [itm, ...target];
+    source = source.filter((_, i) => i != r);
+  }
+  next();
 </script>
 
 <h3>
   <div><Timer /></div>
-  <!-- <div>Задано:</div> -->
-  <div># {rand + 1} &nbsp; Повтор: {count}</div>
-  <!-- <div>Статистика.</div> -->
+  <div>Вопросы: {source.length} / {target.length}</div>
 </h3>
 
-<h1>{topic.name}</h1>
+{#if source.length > 0}
+  <h1>{topic}</h1>
+{:else}
+  <h3>Сказочке конец.</h3>
+{/if}
 
-<button onclick={re} bind:this={btn}>Обновить</button>
+<button onclick={next} bind:this={btn}>Дальше</button>
 
 <style>
   h3 {
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,32 +1,55 @@
 <script lang="ts">
-  // Текущее время в секундах
   let seconds = $state(0);
-  let mins = $state("00");
-  let secs = $state("00");
+  let limit = 60;
+  let h = $state("0");
+  let m = $state("00");
+  let s = $state("00");
 
-  // Эффект для запуска таймера
+  let hv = $derived(h == "0" ? "" : h + ":");
+
   $effect(() => {
-    const interval = setInterval(() => {
-      seconds += 1;
-      mins = Math.floor(seconds / 60)
-        .toString()
-        .padStart(2, "0");
-      secs = (seconds % 60).toString().padStart(2, "0");
-    }, 1000);
-
-    // Очистка при размонтировании
+    const interval = setInterval(updateDisplay, 1000);
     return () => clearInterval(interval);
   });
+
+  function updateDisplay() {
+    seconds += 1;
+    h = Math.floor(seconds / (limit * limit))
+      .toString()
+      .at(-1) as string;
+    m = Math.floor(seconds / limit)
+      .toString()
+      .padStart(2, "0");
+    s = (seconds % limit).toString().padStart(2, "0");
+  }
+
+  function re(e: Event) {
+    e.preventDefault();
+    seconds = -1;
+    updateDisplay();
+  }
 </script>
 
-<span><b>{mins}</b><i>:{secs}</i></span>
+<a href="?" onclick={re}>
+  <b>{hv}</b>{m}<i>:{s}</i>
+</a>
 
 <style>
-  span {
+  a {
+    font-family: monospace;
+    font-size: 1.3em;
     letter-spacing: 0.1ch;
+    padding: 3px 0.5ch 3px 1ch;
+    border-radius: 3px;
+    background-color: #540c0c9f;
+    color: white;
+    opacity: 0.8;
   }
-  span i {
+  a:active {
+    color: #540c0c9f;
+  }
+  i {
     font-style: normal;
-    opacity: 0.2;
+    opacity: 0.3;
   }
 </style>
