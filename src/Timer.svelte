9a31c64afba469ab69677cd39e1b9e2491364b1a
vodish <vodish@yandex.ru>
2026-02-14 15:10:46 +0300

    add Timer

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,33 +1,32 @@
 <script lang="ts">
-    // Текущее время в секундах
-    let seconds = $state(0);
+  // Текущее время в секундах
+  let seconds = $state(0);
+  let mins = $state("00");
+  let secs = $state("00");
 
-    // Форматирование в MM:SS
-    let formattedTime = $derived.by(() => {
-        const mins = Math.floor(seconds / 60)
-            .toString()
-            .padStart(2, "0");
-        const secs = (seconds % 60).toString().padStart(2, "0");
-        return `${mins}:${secs}`;
-    });
+  // Эффект для запуска таймера
+  $effect(() => {
+    const interval = setInterval(() => {
+      seconds += 1;
+      mins = Math.floor(seconds / 60)
+        .toString()
+        .padStart(2, "0");
+      secs = (seconds % 60).toString().padStart(2, "0");
+    }, 1000);
 
-    // Эффект для запуска таймера
-    $effect(() => {
-        const interval = setInterval(() => {
-            seconds += 1;
-        }, 1000);
-
-        // Очистка при размонтировании
-        return () => clearInterval(interval);
-    });
+    // Очистка при размонтировании
+    return () => clearInterval(interval);
+  });
 </script>
 
-<span>
-    {formattedTime}
-</span>
+<span><b>{mins}</b><i>:{secs}</i></span>
 
 <style>
-    span {
-        letter-spacing: 0.2ch;
-    }
+  span {
+    letter-spacing: 0.1ch;
+  }
+  span i {
+    font-style: normal;
+    opacity: 0.2;
+  }
 </style>
